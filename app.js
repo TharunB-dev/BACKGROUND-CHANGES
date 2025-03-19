06a38e5edac50275c651c7923c6d3eb94f20f6fd
TharunB-dev <tharunmec24@gmail.com>
2025-03-19 20:28:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var body=document.querySelector("body");
+
+function dark_on(){
+    body.className="";
+    body.classList.add("dark");
+}
+
+function light_on(){
+    body.className="";
+    body.classList.add("light");
+}
